b8cd7a4915463bd03886f5e704ce616099a3709d
Felipe <fenemeth@gmail.com>
2024-07-21 15:52:28 -0300

    Reorganized project structure to move Git to the main folder

---
+++ src/main/java/com/example/nmth_srv_hello_world/NmthSrvHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.nmth_srv_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmthSrvHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmthSrvHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nmth_srv_hello_world/NmthSrvHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nmth_srv_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmthSrvHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
